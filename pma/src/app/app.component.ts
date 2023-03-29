93797f5af7c9db7ecd33f0c6440e71980895e0ec
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-03-30 00:06:32 +0300

    feat: add form with todolist to app component

--- pma/src/app/app.component.ts
+++ pma/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pma-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pma';
+  todoItemName = '';
+  todoList: TodoItem[] =[];
+  idCounter = 0;
+  constructor(){
+
+  }
+  onSubmit(){
+    this.todoList.push({
+      id: this.idCounter,
+      title: this.todoItemName,
+      isCompleted: false,
+    });
+    this.idCounter++;
+    this.todoItemName = '';
+  }
+  toggleComplete(item:TodoItem){
+    item.isCompleted = !item.isCompleted;
+    console.log(this.todoList)
+  }
 }
