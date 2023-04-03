cba705045a100c7884d795048fc9667151d9c28d
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-04-03 23:53:20 +0300

    feat: add login template, change sign up errors

--- pma/src/app/auth/pages/login-page/login-page.component.ts
+++ pma/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'pma-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
-
+export class LoginPageComponent implements OnInit{
+  errorMessage = '';
+  errorMessageShow = false;
+  constructor(){}
+  loginForm = new FormGroup({
+    login: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+  ngOnInit(): void {
+  }
+  loginSubmit(){
+    if (this.loginForm.valid) {
+ console.log(this.loginForm.value)
+    }else {
+      this.errorMessageShow = true;
+      this.errorMessage = 'All field required!'
+    }
+  }
+  getErrorMessage() {
+    return (this.loginForm.value.password!.length < 7) ? 'Password is required and should be min 7 chars' : '';
+  }
 }
