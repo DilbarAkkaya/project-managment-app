15a9e2e6d1bbc9d2bce77eabb4e2b7d541c0cdbe
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-04-08 18:01:00 +0300

    fix: fix redirect to main if is authenticated

--- pma/src/app/app.component.ts
+++ pma/src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthserviceService } from './auth/services/authservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pma-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+
+  constructor(private auth: AuthserviceService, private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    if (this.auth.isAuthenticated()) {
+      this.router.navigate(['./auth/main']);
+    }
+  }
   title = 'RMA';
 
 }
