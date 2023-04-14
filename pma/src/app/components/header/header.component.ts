fa36e3c750d631872f26cbc5ed8add0e8759785e
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-04-14 12:00:16 +0300

    fix: fix route when click crete board

--- pma/src/app/components/header/header.component.ts
+++ pma/src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthserviceService } from 'src/app/auth/services/authservice.service';
 import { ModalCreateComponent } from 'src/app/auth/components/modal-create/modal-create.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pma-header',
@@ -9,10 +10,10 @@ import { ModalCreateComponent } from 'src/app/auth/components/modal-create/modal
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(public apiservice: AuthserviceService, private dialog: MatDialog) {}
+  constructor(public apiservice: AuthserviceService, private dialog: MatDialog, private router: Router) {}
 
   openCreateDialog() {
-
+    this.router.navigate(['./auth/main']);
     const dialogRef = this.dialog.open(ModalCreateComponent);
 
     dialogRef.afterClosed().subscribe(result => {
