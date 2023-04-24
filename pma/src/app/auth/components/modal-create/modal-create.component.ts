e05927f5778e8015b7f924033d87e9c9a8fce0da
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-04-24 22:48:28 +0300

    feat: add success snackbar for taskupdate, refactor modal create and task

--- pma/src/app/auth/components/modal-create/modal-create.component.ts
+++ pma/src/app/auth/components/modal-create/modal-create.component.ts
@@ -1,32 +1,27 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 import { BoardserviceService } from '../../services/board.service';
 import { IBoardCreate, IBoardResponse } from 'src/app/models/api.model';
-import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'pma-modal-create',
   templateUrl: './modal-create.component.html',
   styleUrls: ['./modal-create.component.scss']
 })
-export class ModalCreateComponent implements OnInit{
-  boards: IBoardResponse[] =[];
+export class ModalCreateComponent implements OnInit {
+  boards: IBoardResponse[] = [];
   constructor(private dialogRef: MatDialogRef<ModalCreateComponent>, private boardservice: BoardserviceService) { }
   ngOnInit(): void {
-
   }
   errorMessage = '';
   errorMessageShow = false;
   createForm = new FormGroup({
     title: new FormControl('', Validators.required),
-/*     login: new FormControl('', Validators.required),
-    password: new FormControl('', [Validators.required, Validators.minLength(7)]) */
   })
-   close() {
+  close() {
     this.dialogRef.close();
   }
-
   createSubmit(): void {
     if (this.createForm.valid) {
       const board: IBoardCreate = {
@@ -34,29 +29,10 @@ export class ModalCreateComponent implements OnInit{
         owner: localStorage.getItem('owner') || null || undefined,
         users: ['']
       }
-        this.boardservice.createBoard(board).subscribe((board) => {
-          this.boards.push(board)
-          this.dialogRef.close({ clicked: 'submit', form: this.createForm });
-        })
-      }
-/*
-
-      this.boardservice.createBoard(board).subscribe((response: any) => {
-        console.log('Form data submitted successfully:', response);
+      this.boardservice.createBoard(board).subscribe((board) => {
+        this.boards.push(board)
         this.dialogRef.close({ clicked: 'submit', form: this.createForm });
-      }, (error: any) => {
-        console.error('Error submitting form data:', error);
-      });
-    } */
+      })
+    }
   }
 }
-
-/* this.boardservice.createBoard(board).pipe(
-  switchMap(() => this.boardservice.getAllBoards())
-).subscribe((boards) => {
-  this.boards = boards as IBoardResponse[];
-  this.dialogRef.close({ clicked: 'submit', form: this.createForm });
-}, (error: any) => {
-  console.error('Error submitting form data:', error);
-});
-} */
--- pma/src/app/auth/components/task/task.component.ts
+++ pma/src/app/auth/components/task/task.component.ts
@@ -1,23 +1,24 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IColumnResponse, ITaskResponse } from 'src/app/models/api.model';
 import { BoardserviceService } from '../../services/board.service';
-import { ActivatedRoute, Params } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { TaskUpdateComponent } from '../task-update/task-update.component';
 import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'pma-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss']
 })
-export class TaskComponent implements OnInit{
-  @Input() task: ITaskResponse|undefined;
+export class TaskComponent implements OnInit {
+  @Input() task: ITaskResponse | undefined;
   @Input() column: IColumnResponse | undefined;
   @Input() boardId: string = '';
-  @Input() tasks: ITaskResponse[] =[];
+  @Input() tasks: ITaskResponse[] = [];
   @Output() removeTaskEvent = new EventEmitter<string>();
-  constructor(private route: ActivatedRoute, private boardservice: BoardserviceService, private dialog: MatDialog){
+  constructor(private route: ActivatedRoute, private snackBar: MatSnackBar, private translate: TranslateService, private boardservice: BoardserviceService, private dialog: MatDialog) {
   }
   ngOnInit(): void {
   }
@@ -27,39 +28,20 @@ export class TaskComponent implements OnInit{
   }
   openTaskForm(id: string) {
     const dialogRef = this.dialog.open(TaskUpdateComponent, {
-      data: {boardId: this.boardId, columnId: this.column!._id, id }
+      data: { boardId: this.boardId, columnId: this.column!._id, id }
     });
     dialogRef.afterClosed().subscribe((result) => {
-      console.log(result);
       if (result && result.task) {
         const taskIndex = this.tasks.findIndex((t) => t._id === result.task._id);
         if (taskIndex >= 0) {
           this.tasks[taskIndex] = result.task;
+          this.snackBar.open(this.translate.instant('success.message'), this.translate.instant('success.close'), {
+            duration: 2000,
+            horizontalPosition: 'right',
+            panelClass: ['success-snack'],
+          })
         }
       }
     });
   }
-/*
-  removeTask(id: string){
-    this.boardservice.deleteTaskById(this.boardId, this.column!._id, id).subscribe(()=>{
-      console.log('delete', this.tasks)
-      this.tasks = this.tasks.filter((task)=>{
-        console.log(task, '5555')
-        task._id !== id})
-    })
-  } */
 }
-
-/*     this.route.params.pipe(
-      switchMap((params: Params) => {
-        this.boardId = params['id'];
-        return this.boardservice.getColumnById(this.boardId, this.column!._id).pipe(
-          switchMap((column: IColumnResponse) => {
-            this.column = column;
-            return this.boardservice.getAllTasks(this.boardId, this.column._id);
-          })
-        );
-      })
-    ).subscribe((task) => {
-      this.tasks =task;
-    }); */
