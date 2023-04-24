6f2f07df22b642a3c847f34021ea10d91c3f0d83
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-04-25 00:09:26 +0300

    refactor: delete logs, add catcherrors in auth and board services

--- pma/src/app/auth/services/authservice.service.ts
+++ pma/src/app/auth/services/authservice.service.ts
@@ -11,8 +11,7 @@ export class AuthserviceService {
   public error$: Subject<string> = new Subject<string>()
   constructor(private http: HttpClient, private translate: TranslateService) { }
   apiUrl = apiEnum.base;
-
-  get token(): string | null{
+  get token(): string | null {
     const tokenExp = localStorage.getItem('token-exp');
     if (!tokenExp) {
       this.logout();
@@ -25,58 +24,42 @@ export class AuthserviceService {
     }
     return tokenExp;
   }
-
-  signup(data:ISignData):Observable<INewUser | null>{
-    console.log(data)
+  signup(data: ISignData): Observable<INewUser | null> {
     return this.http.post<INewUser>(`${this.apiUrl}/${apiEnum.signup}`, data).pipe(tap(this.setUserId));
   }
-
   login(data: IAuthData): Observable<IAuthResponse | null> {
-    console.log(data)
     return this.http.post<IAuthResponse>(`${this.apiUrl}/${apiEnum.login}`, data).pipe(tap(this.setToken), catchError(this.handleError.bind(this)))
   }
   private handleError(error: HttpErrorResponse) {
-   const errorMessage = error.error.message;
-   let translatedError = errorMessage;
+    const errorMessage = error.error.message;
+    let translatedError = errorMessage;
 
-   if (errorMessage === 'Authorization error') {
-    console.log('AUTO', translatedError)
-    this.translate.get('errors.autorization').subscribe((response: string) => {
-      translatedError = response;
-
-    })
-  }
-   /*  this.error$.next('Incorrect login or password') */
-   if (errorMessage === 'Bad request') {
-    console.log('bad', translatedError)
-    /* this.error$.next('Something went wrong') */;
-    this.translate.get('errors.badRequest').subscribe((response: string) => {
-      translatedError = response;
-    })
-  }
+    if (errorMessage === 'Authorization error') {
+      this.translate.get('errors.autorization').subscribe((response: string) => {
+        translatedError = response;
+      })
+    }
+    if (errorMessage === 'Bad request') {
+      this.translate.get('errors.badRequest').subscribe((response: string) => {
+        translatedError = response;
+      })
+    }
     this.error$.next(translatedError)
-   return throwError(() => error);
+    return throwError(() => error);
   }
-
-  logout(){
-  this.setToken(null);
-  localStorage.removeItem('user');
+  logout() {
+    this.setToken(null);
+    localStorage.removeItem('user');
   }
   isAuthenticated(): boolean {
     return !!this.token;
   }
-
   private setToken(response: IAuthResponse | null) {
-
     if (response) {
-      console.log(response, '111111111111')
       const expiry = (JSON.parse(atob(response.token.split('.')[1])));
       const expirationTimeInMilliseconds = new Date(expiry.exp * 1000).getTime();
       const expDate = new Date(new Date().getTime() + expirationTimeInMilliseconds);
       const userId = expiry.id;
-
-
-      console.log(userId, '555555555555555555555')
       localStorage.setItem('token', response.token);
       localStorage.setItem('token-exp', expDate.toString());
       localStorage.setItem('owner', userId);
@@ -88,8 +71,7 @@ export class AuthserviceService {
     }
   }
   private setUserId = (response: INewUser | null) => {
-    if (response){
-      console.log('dkdkkdk', response)
+    if (response) {
       localStorage.setItem('owner', response._id.toString());
     }
   }
--- pma/src/app/auth/services/board.service.ts
+++ pma/src/app/auth/services/board.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { IBoardCreate, IBoardResponse, IColumnCreate, IColumnResponse, ITaskCreate, ITaskResponse, IUpdateTask, apiEnum } from 'src/app/models/api.model';
-import {catchError, map, tap} from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,95 +14,101 @@ export class BoardserviceService {
     return this._refresh$.asObservable();
   }
   createBoard(data: IBoardCreate): Observable<IBoardResponse> {
-    console.log(data)
-    return this.http.post<IBoardResponse>(`${this.apiUrl}/${apiEnum.board}`, data, {headers: new HttpHeaders().set("Authorization", `Bearer ${localStorage.getItem('token')}`)}).pipe(
-      tap(()=>{
+    return this.http.post<IBoardResponse>(`${this.apiUrl}/${apiEnum.board}`, data, { headers: new HttpHeaders().set("Authorization", `Bearer ${localStorage.getItem('token')}`) }).pipe(
+      tap(() => {
         this._refresh$.next(true);
       })
     );
   }
-  getAllBoards(): Observable<IBoardResponse[]>{
-    return this.http.get<IBoardResponse[]>(`${this.apiUrl}/boards`)
-     }
+  getAllBoards(): Observable<IBoardResponse[]> {
+    return this.http.get<IBoardResponse[]>(`${this.apiUrl}/boards`).pipe(
+      catchError(error => {
+        return throwError(()=>error);
+      })
+    );
+  }
   getBoardById(id: string | undefined) {
-    return this.http.get<IBoardResponse>(`${this.apiUrl}/boards/${id}`)
+    return this.http.get<IBoardResponse>(`${this.apiUrl}/boards/${id}`).pipe(
+      catchError(error => {
+        return throwError(()=>error);
+      })
+    );
   }
   createColumn(boardId: string, data: IColumnCreate): Observable<IColumnResponse> {
-    console.log(data, 'creating')
-    console.log(boardId)
     return this.http.post<IColumnResponse>(`${this.apiUrl}/${apiEnum.board}/${boardId}/columns`, data).pipe(
-      tap(()=>{
+      tap(() => {
         this._refresh$.next(true);
       })
     )
   }
   getAllColumns(boardId: string): Observable<IColumnResponse[]> {
     return this.http.get<IColumnResponse[]>(`${this.apiUrl}/${apiEnum.board}/${boardId}/columns`).pipe(
-      tap(columns => console.log('Columns:', columns)),
       catchError(error => {
-        console.error(error);
-        return throwError(error);
+        return throwError(() => error);
       })
     );
   }
   getColumnById(boardId: string, columnId: string) {
-    console.log(boardId, columnId)
-    return this.http.get<IColumnResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}`)
-  }
-  createTask(boardId: string, columnId: string, data: ITaskCreate): Observable<ITaskResponse> {
-    console.log(data, 'creating')
-    return this.http.post<ITaskResponse>(`${this.apiUrl}/${apiEnum.board}/${boardId}/columns/${columnId}/tasks`, data)/* .pipe(
-      tap(()=>{
-      this._refresh$.next(true);
-      })
-    ) */
-  }
-  getAllTasks(boardId: string, columnId: string): Observable<ITaskResponse[]> {
-    return this.http.get<ITaskResponse[]>(`${this.apiUrl}/${apiEnum.board}/${boardId}/columns/${columnId}/tasks`).pipe(
-      tap(tasks => console.log('777777777777tasks:',tasks)),
+    return this.http.get<IColumnResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}`).pipe(
       catchError(error => {
-        console.error(error);
-        return throwError(error);
+        return throwError(() => error);
       })
     );
   }
-  getTaskById(boardId: string, columnId: string, taskId:string) {
-    return this.http.get<ITaskResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}/tasks/${taskId}`)
+  createTask(boardId: string, columnId: string, data: ITaskCreate): Observable<ITaskResponse> {
+    return this.http.post<ITaskResponse>(`${this.apiUrl}/${apiEnum.board}/${boardId}/columns/${columnId}/tasks`, data).pipe(
+      catchError(error => {
+        return throwError(() => error);
+      })
+    );
   }
-  deleteTaskById(boardId: string, columnId: string, taskId:string) {
-    return this.http.delete<ITaskResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}/tasks/${taskId}`)/* .pipe(
-      tap(()=>{
-        this._refresh$.next(true);
-        })
-    ) */
+  getAllTasks(boardId: string, columnId: string): Observable<ITaskResponse[]> {
+    return this.http.get<ITaskResponse[]>(`${this.apiUrl}/${apiEnum.board}/${boardId}/columns/${columnId}/tasks`).pipe(
+      catchError(error => {
+        return throwError(() => error);
+      })
+    );
+  }
+  getTaskById(boardId: string, columnId: string, taskId: string) {
+    return this.http.get<ITaskResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}/tasks/${taskId}`).pipe(
+      catchError(error => {
+        return throwError(()=>error);
+      })
+    );
+  }
+  deleteTaskById(boardId: string, columnId: string, taskId: string) {
+    return this.http.delete<ITaskResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}/tasks/${taskId}`).pipe(
+      catchError(error => {
+        return throwError(()=>error);
+      })
+    );
   }
   deleteBoardById(boardId: string) {
-    return this.http.delete<IBoardResponse>(`${this.apiUrl}/boards/${boardId}`)/* .pipe(
-      tap(()=>{
-        this._refresh$.next(true);
-        })
-    ) */
+    return this.http.delete<IBoardResponse>(`${this.apiUrl}/boards/${boardId}`).pipe(
+      catchError(error => {
+        return throwError(()=>error);
+      })
+    );
   }
   deleteColumnById(boardId: string, columnId: string) {
-    return this.http.delete<IBoardResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}`)/* .pipe(
-      tap(()=>{
-        this._refresh$.next(true);
-        })
-    ) */
-      }
-    updateTaskById(boardId: string, columnId: string, taskId: string, data: IUpdateTask) {
-      console.log(columnId)
-      return this.http.put<ITaskResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}/tasks/${taskId}`, data)/* .pipe(
-        tap(()=>{
-          this._refresh$.next(true);
-          })
-      ) */
+    return this.http.delete<IBoardResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}`).pipe(
+      catchError(error => {
+        return throwError(()=>error);
+      })
+    );
   }
-  updateColumnById(boardId: string, columnId: string,  data: IColumnCreate) {
-    return this.http.put<IColumnResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}`, data)/* .pipe(
-      tap(()=>{
-        this._refresh$.next(true);
-        })
-    ) */
-}
+  updateTaskById(boardId: string, columnId: string, taskId: string, data: IUpdateTask) {
+    return this.http.put<ITaskResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}/tasks/${taskId}`, data).pipe(
+      catchError(error => {
+        return throwError(()=>error);
+      })
+    );
+  }
+  updateColumnById(boardId: string, columnId: string, data: IColumnCreate) {
+    return this.http.put<IColumnResponse>(`${this.apiUrl}/boards/${boardId}/columns/${columnId}`, data).pipe(
+      catchError(error => {
+        return throwError(()=>error);
+      })
+    );
+  }
 }
